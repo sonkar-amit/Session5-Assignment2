1db9e30a54a32a53a59c9a61f4421389d9e213a6
sonkar-amit <sonkar.amit08@gmail.com>
2016-12-09 20:32:21 +0530

    JS Assignment2

---
+++ Assignment2/app.js
@@ -0,0 +1,10 @@
+var myDetailObj = {"name":"Amit",
+                    "age":27,
+                    "dob":"23-April-1989",
+                    "placeOfBirth":"Kolkata"
+                  };
+
+console.log(myDetailObj.name);
+console.log(myDetailObj.age);
+console.log(myDetailObj.dob);
+console.log(myDetailObj.placeOfBirth);
